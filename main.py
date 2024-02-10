f7205c0416dfda2cb67119d5db4c5b03e6bf2937
kirilt2 <140178026+kirilt2@users.noreply.github.com>
2024-02-10 16:31:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,67 @@
-import json
-import time
-import requests
-import base64
-from PIL import Image
-from io import BytesIO
 import telebot
-import os
-
-class Text2ImageAPI:
-    def __init__(self, url, api_key, secret_key):
-        self.URL = url
-        self.AUTH_HEADERS = {
-            'X-Key': f'Key {api_key}',
-            'X-Secret': f'Secret {secret_key}',
-        }
-
-    def get_model(self):
-        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
-        data = response.json()
-        return data[0]['id']
-
-    def generate(self, prompt, model, images=1, width=1024, height=1024):
-        params = {
-            "type": "GENERATE",
-            "numImages": images,
-            "width": width,
-            "height": height,
-            "generateParams": {
-                "query": f"{prompt}"
-            }
-        }
-
-        data = {
-            'model_id': (None, model),
-            'params': (None, json.dumps(params), 'application/json')
-        }
-        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
-        data = response.json()
-        return data['uuid']
-
-    def check_generation(self, request_id, attempts=10, delay=10):
-        while attempts > 0:
-            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
-            data = response.json()
-            if data['status'] == 'DONE':
-                return data['images']
-            attempts -= 1
-            time.sleep(delay)
-
-    def decode_base64(self, base64_string, img_name):
-        decoded_data = base64.b64decode(base64_string)
-        image = Image.open(BytesIO(decoded_data))
-        image.save(os.path.join('images', img_name))
+import requests
 
 
-api = Text2ImageAPI('https://api-key.fusionbrain.ai/', '5A80D4969512A685021A8D044F228AB7', '685B956365CD051B2714A6D1B0B52E6D')
-model_id = api.get_model()
+TOKEN = ''
+API = 'https://api.exchangerate-api.com/v4/latest/'
+bot = telebot.TeleBot(TOKEN)
 
 
-if not os.path.exists('images'):
-    os.makedirs('images')
+SUPPORTED_CURRENCIES = {
+    'USD': 'Доллар США',
+    'EUR': 'Евро',
+    'GBP': 'Фунт стерлингов Великобритании',
+    'JPY': 'Японская иена',
+    'AUD': 'Австралийский доллар',
+    'CAD': 'Канадский доллар',
+    'CHF': 'Швейцарский франк',
+    'CNY': 'Китайский юань',
+    'SEK': 'Шведская крона',
+    'NZD': 'Новозеландский доллар',
+    'KRW': 'Южнокорейская вона',
+    'SGD': 'Сингапурский доллар',
+    'NOK': 'Норвежская крона',
+    'MXN': 'Мексиканское песо',
+    'INR': 'Индийская рупия',
+    'RUB': 'Российский рубль',
+    'ZAR': 'Южноафриканский рэнд',
+    'BRL': 'Бразильский реал',
+    'ILS': 'Израильский шекель',
+}
 
 
-bot = telebot.TeleBot("")
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет Напишите мне и я сгенерирую для вас изображение.")
-
-@bot.message_handler(func=lambda message: True)
-def generate_image(message):
-    chat_id = message.chat.id
-    text = message.text
+@bot.message_handler(commands=['help', 'start', 'info'])
+def start(messges):
+    list = "\n".join(f"{code}: {name}" for code, name in SUPPORTED_CURRENCIES.items())
+    bot.reply_to(messges, f"Доступная валюта: {list} \n Используйте команду /convert Сумма денег  Название валюты И в какую валюту К примеру:\n /convert 100 USD EUR")
 
 
-    bot.send_chat_action(chat_id, 'typing')
+    @bot.message_handler(commands=['convert'])
+    def Cmoeny(messges):
+        try:
+            command_parts = messges.text.split()
+            if len(command_parts) != 4:
+                raise ValueError("Неправильный формат наверно Попробуйте: /convert Сумма денег  Название валюты И в какую валюту К примеру: \n /convert 100 USD EUR")
+            amout = float(command_parts[1])
+            Cfrom = command_parts[2].upper()
+            Cto = command_parts[3].upper()
+
+            exchange_rate = get_exchange_rate(Cfrom, Cto)
+            if exchange_rate is None:
+                raise ValueError(f"Валюта не подержанная Посмотрите какие валюты есть: {list}")
+
+            converted_amount = amout * exchange_rate
+            bot.reply_to(messges, f" {amout} {Cfrom} Равно {converted_amount:.2f} {Cto}")
+
+        except Exception as e:
+            bot.reply_to(messges, f"Eroor {str(e)}")
+
+    def get_exchange_rate(Cfrom, Cto):
+        try:
+            response = requests.get(Api + Cfrom)
 
 
-    uuid = api.generate(text, model_id)
-    images = api.check_generation(uuid)[0]
-    api.decode_base64(images, "123.jpg")
-    photo = open(os.path.join('images', "123.jpg"), "rb")
-    bot.send_photo(chat_id, photo)
 
-
-    os.remove(os.path.join('images', "123.jpg"))
 
 
 bot.polling()
