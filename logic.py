d6bd7fc2fdf8d5e7a1d4d09f347cfd75d9e95e01
kirilt2 <140178026+kirilt2@users.noreply.github.com>
2024-08-27 15:32:49 +0300

    Update logic.lua

--- logic.py
+++ logic.py
@@ -1,1 +1,23 @@
 
+local player = game.Players.LocalPlayer
+local gamePassID = 32123423423 
+local textLabel = script.Parent.Parent.TextLabel
+local textButton = script.Parent
+
+local function updateGamePassStatus()
+    if player:HasPass(gamePassID) then
+        textLabel.Text = "Owned"
+        textButton.Visible = false
+    else
+        textLabel.Text = "Buy Max Admin (EARLY ACCESS)"
+        textButton.Visible = true
+    end
+end
+
+textButton.MouseButton1Click:Connect(function()
+    if not player:HasPass(gamePassID) then
+        game:GetService("MarketplaceService"):PromptGamePassPurchase(player, gamePassID)
+    end
+end)
+
+updateGamePassStatus()
